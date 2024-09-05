887cc90971c7b546ae6e2ec93105ce70fc40a9af
Noel Pérez Villanueva <noelp2317@gmail.com>
2024-09-05 13:40:16 +0200

    Agregado contexto, componentes, hooks y store

---
+++ pruebas/01-reading-list/rimisszoic/src/components/BookContent.jsx
@@ -0,0 +1,7 @@
+import { shallow } from 'zustand/shallow'
+
+import { useUserLists } from '../store/userLists'
+import styles from '../styles/main.module.css'
+import { findListDetails } from '../utils/lists'
+import { ActionListButton } from './ActionListButton'
+
---
+++ pruebas/01-reading-list/rimisszoic/src/store/userLists.jsx
@@ -0,0 +1,4 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { toast } from 'sonner'
+import { ACTIONS_MSGS, BookNotification } from '../components/BookNotification'
