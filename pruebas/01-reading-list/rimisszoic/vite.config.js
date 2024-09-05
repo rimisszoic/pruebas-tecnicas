e0b5c4c515ddc0df2d8c825129d87e43088e8487
Noel Pérez Villanueva <noelp2317@gmail.com>
2024-09-05 12:59:25 +0200

    Agregar archivos de configuración de Vite y Jest

---
+++ pruebas/01-reading-list/rimisszoic/vite.config.js
@@ -0,0 +1,15 @@
+import React from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'happy-dom',
+        setupFiles: ['./vitest-setup.js'],
+        alias: {
+            '@/': new URL('./src/', 'http://localhost:3000/').pathname
+        },
+        css: true
+    }
+})
