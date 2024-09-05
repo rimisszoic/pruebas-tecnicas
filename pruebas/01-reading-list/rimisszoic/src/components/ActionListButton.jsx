1c8627a920ec1c203f9a4a0ecf91fd6211e373b7
Noel Pérez Villanueva <noelp2317@gmail.com>
2024-09-05 13:00:12 +0200

    Agregado los test a <ActionListButton />

---
+++ pruebas/01-reading-list/rimisszoic/src/components/ActionListButton.jsx
@@ -0,0 +1,40 @@
+import styles from "../styles/main.module.css";
+import { ACTIONS_MSGS } from "../constants";
+
+const { IS_ALREADY_IN_LIST, ADD_TO_SPECIFIC_LIST, DELETE_FROM_LIST }=ACTIONS_MSGS;
+
+export const TITLE_VARIANTS = {
+  delete: (nameList) => `${DELETE_FROM_LIST} ${nameList}`,
+  isAlready: (nameList) => `${IS_ALREADY_IN_LIST} ${nameList}`,
+  default: (nameList) => `${ADD_TO_SPECIFIC_LIST} ${nameList}`,
+};
+
+export function ActionListButton({
+  Icon,
+  action,
+  variant = "",
+  size = "",
+  nameList = "",
+  extraClassNames = "",
+}) {
+  const classNames = [styles[size], styles[variant], styles[extraClassNames]]
+    .filter(Boolean)
+    .join(" ");
+
+  const title =
+    variant in TITLE_VARIANTS
+      ? TITLE_VARIANTS[variant](nameList)
+      : TITLE_VARIANTS.default(nameList);
+
+  return (
+    <button
+      role="button"
+      aria-label={title}
+      title={title}
+      className={classNames}
+      onClick={action}
+    >
+      <Icon />
+    </button>
+  );
+}
